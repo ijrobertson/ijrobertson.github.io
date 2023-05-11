464260d301bbaf561511afa7d44cbfec3e27849e
ijrobertson <92500579+ijrobertson@users.noreply.github.com>
2023-05-11 13:11:35 -0400

    Updated Line 28 contactform-process.php
    To my email address

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -25,8 +25,8 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
-$Subject = "New message from Aria landing page";
+$EmailTo = "ianjack1643@gmail.com";
+$Subject = "New message from Lingua Bud landing page";
 
 // prepare email body text
 $Body = "";
