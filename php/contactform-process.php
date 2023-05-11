71a7c12cc0ad956bd0729f6e02ee4818524e88c9
ijrobertson <92500579+ijrobertson@users.noreply.github.com>
2023-05-11 13:58:35 -0400

    Updated Line 47 again

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -44,7 +44,7 @@ $Body .= $terms;
 $Body .= "\n";
 
 // send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+$success = mail($EmailTo, $Subject, $Body, "From:".$admin@linguabud.com);
 
 // redirect to success page
 if ($success && $errorMSG == ""){
