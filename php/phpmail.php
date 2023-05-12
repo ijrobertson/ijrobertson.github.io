42240881f1295c2300d25ac64eae1362c1026da2
ijrobertson <92500579+ijrobertson@users.noreply.github.com>
2023-05-12 13:41:09 -0400

    Changed 'Subject'

--- php/phpmail.php
+++ php/phpmail.php
@@ -1,4 +1,4 @@
 <?php
 //sending email with the php mail()
-mail('user@linguabud.com', 'Subject Line Here', 'Body of Message Here', 'From: admin@linguabud.com');
+mail('user@linguabud.com', 'New message from Lingua Bud landing page', 'Body of Message Here', 'From: admin@linguabud.com');
 ?>
