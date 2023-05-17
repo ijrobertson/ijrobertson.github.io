b94fb19824a2346d46906d0acb10ed004fe3b4ad
ijrobertson <92500579+ijrobertson@users.noreply.github.com>
2023-05-17 10:39:39 -0400

    Changed email back to user@...

--- php/phpmail.php
+++ php/phpmail.php
@@ -1,4 +1,4 @@
 <?php
 //sending email with the php mail()
-mail('ianjack1643@gmail.com', 'New message from Lingua Bud landing page', 'Body of Message Here', 'From: admin@linguabud.com');
+mail('user@linguabud.com', 'New message from Lingua Bud landing page', 'Body of Message Here', 'From: admin@linguabud.com');
 ?>
